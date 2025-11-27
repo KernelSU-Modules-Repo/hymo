9af26aacb9d081f7d7fc8926ada34a3b8552a6c4
YuzakiKokuban <heibanbaize@gmail.com>
2025-11-27 20:19:31 +0800

    Revert "feat:Add SuSFS support"
    This reverts commit 5c3d3f5ed79a191ff3a496f522f6038405f21b8e.

--- src/magic_mount/try_umount.rs
+++ src/magic_mount/try_umount.rs
@@ -1,5 +1,5 @@
 #[cfg(any(target_os = "linux", target_os = "android"))]
-use std::{os::fd::RawFd, path::Path, sync::OnceLock, os::unix::ffi::OsStrExt};
+use std::{os::fd::RawFd, path::Path, sync::OnceLock};
 
 #[cfg(any(target_os = "linux", target_os = "android"))]
 use anyhow::Result;
@@ -7,10 +7,6 @@ use anyhow::Result;
 const KSU_INSTALL_MAGIC1: u32 = 0xDEADBEEF;
 const KSU_IOCTL_ADD_TRY_UMOUNT: u32 = 0x40004b12;
 const KSU_INSTALL_MAGIC2: u32 = 0xCAFEBABE;
-
-const CMD_SUSFS_ADD_TRY_UMOUNT: i32 = 0x55580;
-const SUSFS_MAX_LEN_PATHNAME: usize = 256;
-
 #[cfg(any(target_os = "linux", target_os = "android"))]
 static DRIVER_FD: OnceLock<RawFd> = OnceLock::new();
 
@@ -19,12 +15,6 @@ struct KsuAddTryUmount {
     arg: u64,
     flags: u32,
     mode: u8,
-}
-
-#[repr(C)]
-struct StSusfsTryUmount {
-    target_pathname: [u8; SUSFS_MAX_LEN_PATHNAME],
-    mnt_mode: i32,
 }
 
 fn grab_fd() -> i32 {
@@ -47,11 +37,10 @@ where
     P: AsRef<Path>,
 {
     use std::ffi::CString;
+
     use rustix::path::Arg;
 
-    let target_ref = target.as_ref();
-    let path_str = target_ref.as_str()?;
-    
+    let path_str = target.as_ref().as_str()?;
     let path = CString::new(path_str)?;
     let cmd = KsuAddTryUmount {
         arg: path.as_ptr() as u64,
@@ -60,7 +49,6 @@ where
     };
     let fd = *DRIVER_FD.get_or_init(|| grab_fd());
 
-    // KSU Operation
     unsafe {
         #[cfg(target_env = "gnu")]
         let ret = libc::ioctl(fd as libc::c_int, KSU_IOCTL_ADD_TRY_UMOUNT as u64, &cmd);
@@ -68,47 +56,17 @@ where
         #[cfg(not(target_env = "gnu"))]
         let ret = libc::ioctl(fd as libc::c_int, KSU_IOCTL_ADD_TRY_UMOUNT as i32, &cmd);
 
-        if ret == 0 {
-            log::debug!("KSU: Scheduled unmount for {}", path_str);
+        if ret < 0 {
+            use std::io;
+
+            log::error!("{}", io::Error::last_os_error());
         }
     };
-
-    // SUSFS Operation
-    if let Ok(abs_path) = std::fs::canonicalize(target_ref) {
-        let bytes = abs_path.as_os_str().as_bytes();
-        
-        if bytes.len() < SUSFS_MAX_LEN_PATHNAME {
-            let mut info = StSusfsTryUmount {
-                target_pathname: [0; SUSFS_MAX_LEN_PATHNAME],
-                mnt_mode: 1,
-            };
-            
-            info.target_pathname[..bytes.len()].copy_from_slice(bytes);
-            
-            let mut error: i32 = -1;
-            
-            unsafe {
-                libc::prctl(
-                    KSU_INSTALL_MAGIC1 as libc::c_int, 
-                    CMD_SUSFS_ADD_TRY_UMOUNT as libc::c_ulong, 
-                    &info as *const _ as libc::c_ulong, 
-                    0 as libc::c_ulong, 
-                    &mut error as *mut _ as libc::c_ulong
-                );
-            }
-
-            if error == 0 {
-                log::info!("SUSFS: Added try_umount for {}", abs_path.display());
-            } else {
-                log::debug!("SUSFS: Failed to add try_umount for {}, error: {}", abs_path.display(), error);
-            }
-        }
-    }
 
     Ok(())
 }
 
 #[cfg(not(any(target_os = "linux", target_os = "android")))]
-pub(super) fn send_unmountable<P>(_target: P) -> Result<()> {
+pub(super) fn send_unmountable() {
     unimplemented!()
 }
